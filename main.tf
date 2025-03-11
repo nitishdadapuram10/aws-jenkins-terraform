7b7cea7a1e6777569a3867b76650565d3f8edab2
nitishdadapuram10 <62438926+nitishdadapuram10@users.noreply.github.com>
2025-03-10 20:01:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.5.0"
-    }
-  }
-}
+// terraform {
+ // required_providers {
+  //  aws = {
+    //  source  = "hashicorp/aws"
+  //    version = "5.5.0"
+//    }
+ // }
+//}
 
 provider "aws" {
   region = "ap-south-1"
